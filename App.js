1878eb4337af3ba8e7335c245ca08258e0828276
Developer <developer@ouhk.edu.hk>
2021-02-20 18:32:07 +0900

    upload

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
+  const [name, setName] = useState("Apple"); 
+  const [person, setPerson] = useState({name : "mario", age : 10 });
+
+  const clickHandler = () =>
+  {
+    setName("Banana");
+    setPerson({name : "luigi", age : 11});
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}>      
+
+      <View style={styles.body}>
+        <Text style={styles.textStyle}>Hello World..</Text>
+        <Text>My name is {name}...</Text>
+        <Text>His name is {person.name} and age is {person.age}!</Text>
+      </View>
+
+      <Button title="click me" onPress={clickHandler}/>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +36,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  body:
+  {
+    backgroundColor : "yellow",
+    padding : 20,
+    margin : 10
+  },
+
+  textStyle:
+  {
+    fontSize : 20,
+    fontWeight : "bold"
+  }
+
 });
